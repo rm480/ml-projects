0f475a8022ffcb617b23634facf0ec566aadf213
rm480 <rm480@yandex.ru>
2020-01-14 13:13:09 +0300

    Update mal_score_predictor.py

--- mal_score_predictor.py
+++ mal_score_predictor.py
@@ -1,15 +1,60 @@
-#Prediction of anime score from MyAnimeList database
-
 from jikanpy import Jikan
-import numpy as np
+import pandas as pd
 import time
+import pickle
 jikan = Jikan()
-anime = [None]*2
-anime = jikan.anime(39597) #1-10507 + 30000 smth take ids from seasons below
+#anime = jikan.anime(39597)
+#list.extend
+winter = list()
+spring = list()
+summer = list()
+fall = list()
+years = range(1980,2019)
+for Y in years:
+    time.sleep(4)
+    winter.append(jikan.season(year=2008, season='winter'))
+with open('winter.data', 'wb') as filehandle:
+    pickle.dump(winter, filehandle)
+with open('winter.data', 'rb') as filehandle:
+    winter = pickle.load(filehandle)
+#winter[0]['anime'][0]['mal_id']
+for Y in years:
+    try:
+        spring.append(jikan.season(year=2006, season='spring'))
+    except:
+        pass
+    time.sleep(4)
+with open('spring.data', 'wb') as filehandle:
+    pickle.dump(spring, filehandle)
+for Y in years:
+    time.sleep(4)
+    try:
+        summer.append(jikan.season(year=1993, season='summer'))
+    except:
+        pass
+with open('summer.data', 'wb') as filehandle:
+    pickle.dump(summer, filehandle)
+for Y in years:
+    time.sleep(4)
+    try:
+        fall.append(jikan.season(year=2005, season='fall'))
+    except:
+        pass
+with open('fall.data', 'wb') as filehandle:
+    pickle.dump(fall, filehandle)
 
-years = range(2017,2019)
-winter = [jikan.season(year=Y, season='winter') for Y in years]
-spring = [jikan.season(year=Y, season='spring') for Y in years]
-summer = [jikan.season(year=Y, season='summer') for Y in years]
-fall = [jikan.season(year=Y, season='fall') for Y in years]
-db = pd.DataFrame([winter[0]['anime'], spring[0]['anime'], summer[0]['anime'], fall[0]['anime']])
+
+all_seasons = [winter, spring, summer, fall]
+with open('all_seasons.data', 'wb') as filehandle:
+    pickle.dump(all_seasons, filehandle)
+with open('all_seasons.data', 'rb') as filehandle:
+    all_seasons = pickle.load(filehandle)
+id = list()
+for season in range(3):
+    for year in range(39):
+        for anime in range(len(all_seasons[season][year]['anime'])):
+            id.append(all_seasons[season][year]['anime'][anime]['mal_id'])
+id = pd.DataFrame(id)
+id = pd.DataFrame(id[0].unique())
+with open('id.data', 'wb') as filehandle:
+    pickle.dump(id, filehandle)
