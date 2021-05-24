2f3cf51c9df34fd946406747e786a62fdcbc1398
rm480 <rm480@yandex.ru>
2021-05-24 15:24:17 +0300

    Update hawkes_process.py

--- hawkes_process.py
+++ hawkes_process.py
@@ -1,24 +1,26 @@
+# Parse tweets with the keyword 'unicredit' over a month and get their timestamps. #
 import GetOldTweets3 as got
 import pandas as pd
 tweetCriteria = got.manager.TweetCriteria().setQuerySearch('unicredit') \
-    .setSince("2019-12-20").setUntil("2019-12-21")
+    .setSince("2019-11-20").setUntil("2019-12-21")
 tweet = got.manager.TweetManager.getTweets(tweetCriteria)
 tweet_date = [tweet[i].date for i in range(len(tweet))]
 tweet_date_df = pd.DataFrame(tweet_date)
 tweet_date_df.to_csv(path_or_buf='C:\\Users\\linai\\Documents\\tweets\\@2012.csv', index=False)
 
-#start here
+# Import libraries #
 import numpy as np
 import pandas as pd
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
 time = pd.read_csv('diurtime.csv').to_numpy().transpose().flatten().astype(float)
 
+# Define an exponential kernel function #
 def kernel(x,y,b):
     return np.exp(-b * (x-y))
 
+# Define a negative log-likelihood function of a Hawkes process #
 def myHLL(params, seq, sign=1.0, verbose=False):
-    """ (Negative) Log-likelihood """
     mu, alpha, beta = params[0], params[1], params[2]
     last = seq[-1]
     constant_survival = last * mu
@@ -34,6 +36,7 @@ def myHLL(params, seq, sign=1.0, verbose=False):
     ll = sign * (-constant_survival + (alpha/beta) * updating_survival + occurrence_likelihood)
     return ll
 
+# Maximize the function to find the optimal parameters using L-BFGS-B algorithm #
 def myunivariate(seq, verbose=False):
     params = [0.01, 0.03, 0.04]     #[0.01, 0.03, 0.04]
     bounds = [(0, None), (0, None), (0, None)]
@@ -47,10 +50,12 @@ def myunivariate(seq, verbose=False):
                    })
     print(res)
     return res.x[0], res.x[1], res.x[2]
+
+#Print the optimal parameters and corresponding likelihood
 coeffs = myunivariate(time)
 HLL = myHLL(coeffs, time)
 
-#intensity
+# Construct the intensity function #
 inten = np.zeros(int(np.ceil(time[-1])))
 A, prev = 0, 0
 for t in range(1, int(np.ceil(time[-1]))):
@@ -65,36 +70,7 @@ plt.xlabel('Seasonally adjusted time')
 plt.plot(inten)
 del A, t, prev
 
-#compensator (test)
-compen = np.zeros(int(np.ceil(time[-1])))
-B, prev = 0, 0
-for t in range(1, int(np.ceil(time[-1]))):
-    prev = time[np.where(time < t)] #time[-1] is not here
-    for T_i in prev:
-        B += kernel(t, T_i, coeffs[2]) - 1
-    compen[t] = t*coeffs[0] - (coeffs[1]/coeffs[2])*B
-    B = 0
-fig = plt.figure()
-plt.ylabel('Compensator')
-plt.xlabel('Time in minutes')
-plt.plot(compen)
-df = pd.DataFrame(compen)
-df.to_csv(path_or_buf='C:\\Users\\user\\Documents\\tweets\\adjusted_compensator.csv', index=False)
-
-#short intensity (test)
-inten = np.zeros_like(time)
-A, prev, t = 0, 0, 0
-for curr in time:
-    A = kernel(curr, prev, coeffs[2]) * A
-    inten[t] = coeffs[0] + coeffs[1] * A
-    prev = curr
-    A += 1
-    t += 1
-plt.ylabel('Intensity')
-plt.xlabel('Tweets')
-plt.plot(inten)
-
-#short compensator
+# Construct the compensator function #
 compen = np.zeros_like(time)
 B, prev, t = 0, 0, 0
 for curr in time:
